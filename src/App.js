fba76eb7eecf90043e3c98f1addd8316d0d7ab0c
Stephen Tambussi <stevetambussi@gmail.com>
2022-05-08 18:58:48 -0700

    Started website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import { Route, Routes, Link } from 'react-router-dom';
 //import MainPage from './MainPage';
 
@@ -8,11 +8,36 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
+
         <header className="App-header">
-          <h1 class="Title">Character Generator Title</h1>
-          <button type="download_btn">Download</button>
-          <a href="https://github.com/stephentambussi/char-gen"><button type="gh_btn">Github</button></a>
+          <h1 className="Title">Character Generator Title</h1>
+          <ul className="title_btns">
+            <li className="title_btn"><button type="download_btn">Download</button></li>
+            <li className="title_btn"><a href="https://github.com/stephentambussi/char-gen"><button type="gh_btn">Github</button></a></li>
+          </ul>
         </header>
+
+        <div className="Template">
+          <h2 className="TemplateTitle">Character Template</h2>
+
+          <div className="WorldInfo">
+            <p>
+              <label htmlFor="world_info_textbox" name="WorldInfoTitle">Virtual World Information (REQUIRED)</label>
+            <br />
+              <textarea name="world_info_textbox" rows="6" cols="150" placeholder="Enter details here about the virtual game world that you want a character generated for 
+                (one or more sentences)"> 
+              </textarea>
+            </p>
+          </div>
+
+        </div>
+
+        <div className="Chat">
+          <p>
+            Test text for chat
+          </p>
+        </div>
+
       </div>
       /* Keep this for now as it shows how to add navigation tabs
       <div className="App">
